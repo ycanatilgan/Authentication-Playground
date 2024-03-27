b6d69c2a8254befa6df28b5c2a17e1ab7c1a4e91
ycanindev <ycan_atilgan@hotmail.com>
2024-03-27 22:20:25 +0300

    #DB Context and conn. string typo #User table added to db

--- Authentication Playground_/Data/AppDbContext.cs
+++ Authentication Playground_/Data/AppDbContext.cs
@@ -1,12 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Authentication_Playground_.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Authentication_Playground_.Data
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options)
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
 
         }
+
+        public DbSet<Users> Users { get; set; }
     }
 }
